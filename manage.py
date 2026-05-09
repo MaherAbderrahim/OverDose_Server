f36c652d6e19cee2d9ff48b94cfd8c56c06e3fe3
houssem-bench <houssem.benchaabane@esprit.tn>
2026-05-09 17:13:10 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,10 @@
 import os
 import sys
 import asyncio
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+import sys
+
+if sys.platform.startswith("win"):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 def main():
     """Run administrative tasks."""
